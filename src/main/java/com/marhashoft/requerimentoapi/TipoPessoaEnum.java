28b67cc6943c88f51b8c0feb8103e82f9117b5a8
samuelfer <samuelfesant@gmail.com>
2022-08-13 17:03:14 -0300

    Add enum para TipoPessoa

---
+++ src/main/java/com/marhashoft/requerimentoapi/TipoPessoaEnum.java
@@ -0,0 +1,25 @@
+package com.marhashoft.requerimentoapi;
+
+public enum TipoPessoaEnum {
+
+    TIPO_USUARIO(1L,"Usuário"),
+    TIPO_VEREADOR(2L,"Vereador"),
+    TIPO_SERVIDOR(3L,"Servidor"),
+    TIPO_ASSESSOR(4L,"Assessor");
+
+    private Long id;
+    private String descricao;
+
+    TipoPessoaEnum(Long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
