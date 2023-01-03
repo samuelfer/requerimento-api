15b97c05af0f56fbd35461b92dd32e2e759bef43
samuelfer <samuelfesant@gmail.com>
2023-01-03 18:29:13 -0300

    Remove import de JsonIgnore

--- src/main/java/com/marhashoft/requerimentoapi/model/Assessor.java
+++ src/main/java/com/marhashoft/requerimentoapi/model/Assessor.java
@@ -1,6 +1,5 @@
 package com.marhashoft.requerimentoapi.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
