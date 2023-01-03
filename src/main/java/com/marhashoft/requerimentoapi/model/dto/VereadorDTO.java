8cb9daecebb5d907e22c8ed1dbed908a0d9c2c5b
samuelfer <samuelfesant@gmail.com>
2023-01-03 18:26:34 -0300

    Add dto para vereador

---
+++ src/main/java/com/marhashoft/requerimentoapi/model/dto/VereadorDTO.java
@@ -0,0 +1,28 @@
+package com.marhashoft.requerimentoapi.model.dto;
+
+import com.marhashoft.requerimentoapi.model.Cargo;
+import com.marhashoft.requerimentoapi.model.TipoPessoa;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VereadorDTO {
+
+    private Long id;
+
+    @NotBlank(message = "O campo nome do vereador é obrigatório")
+    private String nome;
+
+    private boolean ativo;
+
+    private TipoPessoa tipoPessoa;
+
+    private Cargo cargo;
+}
