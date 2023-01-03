b27be810ecdb8a30174b8fb986b62aa3da49f9f1
samuelfer <samuelfesant@gmail.com>
2023-01-03 18:30:22 -0300

    Injeta ModelMapper e utiliza  altere para o cadastro e edição do Assessor usar AssessorDTO

--- src/main/java/com/marhashoft/requerimentoapi/controller/AssessorController.java
+++ src/main/java/com/marhashoft/requerimentoapi/controller/AssessorController.java
@@ -1,18 +1,16 @@
 package com.marhashoft.requerimentoapi.controller;
 
 import com.marhashoft.requerimentoapi.model.Assessor;
-import com.marhashoft.requerimentoapi.model.Pessoa;
-import com.marhashoft.requerimentoapi.model.Requerimento;
+import com.marhashoft.requerimentoapi.model.dto.AssessorDTO;
 import com.marhashoft.requerimentoapi.service.AssessorService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/assessores")
@@ -20,6 +18,8 @@ public class AssessorController {
 
     @Autowired
     AssessorService assessorService;
+    @Autowired
+    ModelMapper modelMapper;
 
     @GetMapping
     public List<Assessor> listarPessoas() {
@@ -27,24 +27,20 @@ public class AssessorController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> listarPorId(@PathVariable("id") Long id) {
-        try {
-            Assessor assessor = assessorService.findByIdOuErro(id);
-            return new ResponseEntity<>(assessor, HttpStatus.OK);
-        } catch (Exception e) {
-            Logger.getLogger(e.getMessage());
-            return new ResponseEntity<>(Arrays.asList(e.getMessage()), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<Assessor> listarPorId(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(assessorService.findByIdOuErro(id), HttpStatus.OK);
     }
 
 //    @PreAuthorize("hasAnyRole('USUARIO')")
     @PostMapping
-    public ResponseEntity<Assessor> cadastrar(@Valid @RequestBody Assessor assessor) {
-        return new ResponseEntity(assessorService.salvar(assessor), HttpStatus.CREATED);
+    public ResponseEntity<Assessor> cadastrar(@Valid @RequestBody AssessorDTO assessorDTO) {
+        Assessor assessor = modelMapper.map(assessorDTO, Assessor.class);
+        return new ResponseEntity<>(assessorService.salvar(assessor), HttpStatus.CREATED);
     }
 
     @PutMapping
-    public ResponseEntity<Assessor> atualizar(@Valid @RequestBody Assessor assessor) {
-        return new ResponseEntity(assessorService.atualizar(assessor), HttpStatus.CREATED);
+    public ResponseEntity<Assessor> atualizar(@Valid @RequestBody AssessorDTO assessorDTO) {
+        Assessor assessor = modelMapper.map(assessorDTO, Assessor.class);
+        return new ResponseEntity<>(assessorService.atualizar(assessor), HttpStatus.CREATED);
     }
 }
