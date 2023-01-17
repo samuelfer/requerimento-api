f6ff8aed620ae5f7e4812436be174b7367491b97
samuelfer <samuelfesant@gmail.com>
2023-01-17 18:35:24 -0300

    Add annotation @Repository

--- src/main/java/com/marhashoft/requerimentoapi/repository/ServidorRepository.java
+++ src/main/java/com/marhashoft/requerimentoapi/repository/ServidorRepository.java
@@ -2,9 +2,11 @@ package com.marhashoft.requerimentoapi.repository;
 
 import com.marhashoft.requerimentoapi.model.Servidor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ServidorRepository extends JpaRepository<Servidor, Long> {
 
     Optional<Servidor> findByNome(String nome);
--- src/main/java/com/marhashoft/requerimentoapi/repository/TipoPessoaRepository.java
+++ src/main/java/com/marhashoft/requerimentoapi/repository/TipoPessoaRepository.java
@@ -2,9 +2,11 @@ package com.marhashoft.requerimentoapi.repository;
 
 import com.marhashoft.requerimentoapi.model.TipoPessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TipoPessoaRepository extends JpaRepository<TipoPessoa, Long> {
 
     List<TipoPessoa> findByAtivoTrueOrderByDescricao();
