ebb89402b86e12fcf8194dc916cf2082e27e7ead
samuelfer <samuelfesant@gmail.com>
2023-01-31 21:13:43 -0300

    feat: add repository e model para Role

---
+++ src/main/java/com/marhashoft/requerimentoapi/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.marhashoft.requerimentoapi.repository;
+
+import com.marhashoft.requerimentoapi.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByNome(String nome);
+}
