734de0a802b69964eff50a79012d8115b52c00b4
samuelfer <samuelfesant@gmail.com>
2023-02-11 13:41:36 -0300

    feat: adicione método para listar vereador por assessor

--- src/main/java/com/marhashoft/requerimentoapi/service/VereadorService.java
+++ src/main/java/com/marhashoft/requerimentoapi/service/VereadorService.java
@@ -1,8 +1,11 @@
 package com.marhashoft.requerimentoapi.service;
 
 import com.marhashoft.requerimentoapi.CargoEnum;
+import com.marhashoft.requerimentoapi.TipoPessoaEnum;
 import com.marhashoft.requerimentoapi.exception.DataIntegrationViolationApiException;
+import com.marhashoft.requerimentoapi.model.Assessor;
 import com.marhashoft.requerimentoapi.model.Cargo;
+import com.marhashoft.requerimentoapi.model.Usuario;
 import com.marhashoft.requerimentoapi.model.Vereador;
 import com.marhashoft.requerimentoapi.model.dto.VereadorDTO;
 import com.marhashoft.requerimentoapi.repository.VereadorRepository;
@@ -11,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,12 +29,21 @@ public class VereadorService {
     private TipoPessoaService tipoPessoaService;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private UsuarioService usuarioService;
+    @Autowired
+    private AssessorService assessorService;
 
     public Vereador findByIdOuErro(Long id) {
         return vereadorRepository.findById(id).orElseThrow(() -> new RuntimeException("Vereador não encontrado com id " + id));
     }
 
     public List<Vereador> listarTodos() {
+        Usuario usuario = usuarioService.getusuarioLogadoOuErro();
+
+        if (usuario.getTipoPessoa().getId().equals(TipoPessoaEnum.TIPO_ASSESSOR.getId())) {
+            return listarVereadorPorAssessor(usuario.getPessoaId());
+        }
         return vereadorRepository.findAll(Sort.by(Sort.Direction.DESC, "nome"));
     }
 
@@ -59,5 +72,16 @@ public class VereadorService {
             throw new DataIntegrationViolationApiException("O vereador  "
                     + vereadorDTO.getNome() + " já foi cadastrado no sistema!");
         }
+    }
+
+    private List<Vereador> listarVereadorPorAssessor(Long assessorId) {
+        Assessor assessor = assessorService.findByIdOuErro(assessorId);
+        List<Vereador> vereador = new ArrayList<>();
+        Optional<Vereador> vereadorResult = vereadorRepository.findById(assessor.getVereador().getId());
+        if (!vereadorResult.isPresent()) {
+            throw new RuntimeException("Erro ao tentar listar o vereador");
+        }
+        vereador.add(vereadorResult.get());
+        return vereador;
     }
 }
