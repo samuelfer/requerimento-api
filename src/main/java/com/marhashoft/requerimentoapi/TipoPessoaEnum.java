0fa0c044a874bc757975c27384ded010d610f2cd
samuelfer <samuelfesant@gmail.com>
2023-02-11 13:39:09 -0300

    feat: add constante TIPO_ADMIN

--- src/main/java/com/marhashoft/requerimentoapi/TipoPessoaEnum.java
+++ src/main/java/com/marhashoft/requerimentoapi/TipoPessoaEnum.java
@@ -5,7 +5,8 @@ public enum TipoPessoaEnum {
     TIPO_USUARIO(1L,"Usuário"),
     TIPO_VEREADOR(2L,"Vereador"),
     TIPO_SERVIDOR(3L,"Servidor"),
-    TIPO_ASSESSOR(4L,"Assessor");
+    TIPO_ASSESSOR(4L,"Assessor"),
+    TIPO_ADMIN(5L,"Admin");
 
     private Long id;
     private String descricao;
