37eed2412c3a4b37a0791db34d8fdf7186cf383f
samuelfer <samuelfesant@gmail.com>
2023-02-11 13:40:57 -0300

    feat: adicione todas tabelas

---
+++ src/main/resources/db/migration/V2.0__insert_tipo_pessoa.sql
@@ -0,0 +1,4 @@
+--INSERT INTO public.tipo_pessoa (id,ativo,descricao) VALUES (1,true,'Usuário');
+--INSERT INTO public.tipo_pessoa (id,ativo,descricao) VALUES (2,true,'Vereador');
+--INSERT INTO public.tipo_pessoa (id,ativo,descricao) VALUES (3,true,'Servidor');
+--INSERT INTO public.tipo_pessoa (id,ativo,descricao) VALUES (4,true,'Assessor');
