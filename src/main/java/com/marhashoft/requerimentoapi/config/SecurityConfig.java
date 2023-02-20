4f380d4f6e08ca8b0137c08e39607629f6ffedc9
samuelfer <samuelfesant@gmail.com>
2023-02-20 18:58:08 -0300

    feat: add anotação configuration

--- src/main/java/com/marhashoft/requerimentoapi/config/SecurityConfig.java
+++ src/main/java/com/marhashoft/requerimentoapi/config/SecurityConfig.java
@@ -5,10 +5,10 @@ import com.marhashoft.requerimentoapi.security.JWTUtil;
 import com.marhashoft.requerimentoapi.security.JWTUtilAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,7 +19,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-@EnableWebSecurity
+@Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
