18fd6e7e79566781e6f4ef9e2c0b935fd684cf9b
Samuel <samuelfesant@gmail.com>
2023-12-10 11:55:02 -0300

    Add método para pegar o tipo de arquivo pelo nome

--- src/main/java/com/marhashoft/requerimentoapi/enums/TipoArquivoEnum.java
+++ src/main/java/com/marhashoft/requerimentoapi/enums/TipoArquivoEnum.java
@@ -1,11 +1,23 @@
 package com.marhashoft.requerimentoapi.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum TipoArquivoEnum {
 
     FOTO_VEREADOR("Foto_vereador"),
-    ARQ_ASSINATURA_VEREADOR("Arquivo_assinatura_vereador");
+    ARQ_ASSINATURA_VEREADOR("Arquivo_assinatura_vereador"),
+    LOGO_REQUERIMENTO("Logo_requerimento");
 
     private String nome;
+
+    private static final Map<String, TipoArquivoEnum> tipoArquivoPorNome = new HashMap<>();
+
+    static {
+        for (TipoArquivoEnum tipo : TipoArquivoEnum.values()) {
+            tipoArquivoPorNome.put(tipo.getNome(), tipo);
+        }
+    }
 
     TipoArquivoEnum(String nome) {
         this.nome = nome;
@@ -13,5 +25,9 @@ public enum TipoArquivoEnum {
 
     public String getNome() {
         return nome;
+    }
+
+    public static TipoArquivoEnum recuperarTipoPorNome(String valor) {
+        return tipoArquivoPorNome.get(valor);
     }
 }
